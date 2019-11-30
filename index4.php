20a7dbaf6e7e512fe10c8e92f49f4e4bb29e5b27
Krishna Teja <packetcode@gmail.com>
2019-11-30 15:43:17 +0530

    some changes

--- index4.php
+++ index4.php
@@ -1,36 +1,9 @@
 <?php
+// Code written by Krishna Teja G S
+// packetprep.com
+// packetcode@gmail.com
 
+require 'lib/boot.php';
 
-
-//file_put_contents($file, $output);
-
-$lang = 'java';
-		
-$payload = '{"language":"java","command":"javac Main.java && java Main", "files": [{"name": "Main.java", "content": "class Main{public static void main(String[] args) { for(int i=1;i<5;i++) System.out.println(\"Start\");}}"}]}';
-
-
-$json = run2($lang,$payload);
-
-header('Content-Type: application/json');
-echo $json;
-
-
-function run3(){
-		return shell_exec("bash bash.sh json/b75104c");
-}
-
-function run2($lang,$payload){
-
-	$file = 'json/'.substr(md5(mt_rand()), 0, 7);
-	$filename = $file.'.json';
-	file_put_contents($filename, $payload);
-
-	$cmd = "bash bash.sh ".$file;
-
-	echo $cmd;
-	echo "\n";
-	$output = shell_exec($cmd);
-	//unlink($filename);
-	return $output;
-
-}
+$boot = new Boot();
+$boot->router2(2);
