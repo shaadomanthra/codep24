3cbb8e7adce6cd5a61fafa5194c3f43786973d1f
Krishna Teja <packetcode@gmail.com>
2019-12-14 20:07:08 +0530

    some changes

--- form.php
+++ form.php
@@ -1,36 +1,43 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h2>HTML Forms</h2>
-
+<?php require 'pages/blocks/header.php';  ?>
+<?php  require 'lib/url.php'; ?>
+<main role="main" class="flex-shrink-0">
+  <div class="container">
+<div class="mt-4 bg-light border p-3 rounded">
 <form action="index.php" method="post">
   hash:<br>
-  <input type="text" name="hash" value="krishnateja">
+  <input type="text" class="form-control" name="hash" value="krishnateja">
   <br>
   Docker:<br>
-  <input type="text" name="docker" value="1"><br>
+  <input type="text" class="form-control" name="docker" value="1"><br>
   C:<br>
-  <input type="text" name="c" value="1"><br>
+  <input type="text" class="form-control" name="c" value="1"><br>
   Docker:<br>
-  <input type="text" name="docker" value="1"><br>
+  <input type="text" name="docker" class="form-control" value="1"><br>
   language:<br>
-  <input type="text" name="lang" value="clang">
-  <br><br>
+  <input type="text" name="lang" class="form-control" value="clang">
+  <br>
   code:<br>
-  <textarea name="code">#include&lt;stdio.h> \n int main(void)\n {\n printf(\"Hello World!\");\n return 0;\n}
+  <textarea class="form-control" name="code" id="c-code" >#include<stdio.h>
+int main(void) {
+  printf("Hello World!");
+  return 0;
+}
   </textarea>
   <input type="hidden" name="form" value="1">
   
-  <input type="submit" value="Submit">
+  <input type="submit" class="btn btn-primary mt-4" value="Submit">
 </form> 
-
-<div style="padding:20px; border:1px solid silver;margin: 20px 0px;">
-	<pre>
-<?php if(isset($output)){ 
-	echo $output;
- } ?>
-</pre>
+<script>
+    var cEditor = CodeMirror.fromTextArea(document.getElementById("c-code"), {
+      lineNumbers: true,
+      matchBrackets: true,
+      mode: "text/x-csrc",
+      theme: "material",
+      indentUnit: 4
+    });
+  </script>
 </div>
-</body>
-</html>
+</div>
+</main>
+
+<?php require 'pages/blocks/header.php';  ?>
