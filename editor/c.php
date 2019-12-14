42dfb16784d9d8f519cc1a9240fe4a43f94598e7
Krishna Teja <packetcode@gmail.com>
2019-12-14 16:22:20 +0530

    some changes

--- editor/c.php
+++ editor/c.php
@@ -3,28 +3,18 @@
   <div class="container">
     <h1 class="mt-5 mb-5"><img src="../img/p24.png" width="100px"/></h1>
     <form action="../index.php" method="post"><textarea id="c-code" name="code">#include <stdio.h> 
-      int main(void) {
-      printf("Hello World!\n");
-      return 0; 
-    }</textarea>
+
+int main(void) {
+    printf("Hello World!\n");
+    return 0; 
+}</textarea>
     <input type="hidden" name="hash" value="krishnateja" />
     <input type="hidden" name="lang" value="clang" />
     <input type="hidden" name="page" value="output" />
+    <input type="hidden" name="docker" value="1" />
     <button type="submit" class="btn btn-primary btn-lg mt-4"  role="button" >Run</button>
   </form>
 
-<?php
-	require '../lib/boot.php';
-	$boot = new Boot();
-	$output = $boot->code();
-
-    if($output){ ?>
-  <div class="card">
-    <div class="card-body">
-          echo $output;
-    </div>
-  </div>
-<?php } ?>
   <script>
     var cEditor = CodeMirror.fromTextArea(document.getElementById("c-code"), {
       lineNumbers: true,
---
+++ editor/java.php
@@ -0,0 +1,34 @@
+<?php require '../pages/blocks/header.php';  ?>
+<main role="main" class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5 mb-5"><img src="../img/p24.png" width="100px"/></h1>
+    <form action="../index.php" method="post"><textarea id="java-code" name="code">public class Main {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}</textarea>
+    <input type="hidden" name="hash" value="krishnateja" />
+    <input type="hidden" name="lang" value="java" />
+    <input type="hidden" name="page" value="output" />
+    <input type="hidden" name="docker" value="1" />
+    <button type="submit" class="btn btn-primary btn-lg mt-4"  role="button" >Run</button>
+  </form>
+
+  <script>
+    var javaEditor = CodeMirror.fromTextArea(document.getElementById("java-code"), {
+        lineNumbers: true,
+        matchBrackets: true,
+        mode: "text/x-java",
+        theme: "material",
+          indentUnit: 4
+      });
+  </script>
+  
+</div>
+</main>
+<?php  require '../pages/blocks/footer.php'; ?>
