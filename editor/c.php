76f29897e80f815d1dbbda02e0a03a9bfa77ddfd
Krishna Teja <packetcode@gmail.com>
2019-12-14 15:40:53 +0530

    some changes

--- editor/c.php
+++ editor/c.php
@@ -2,8 +2,7 @@
 <main role="main" class="flex-shrink-0">
   <div class="container">
     <h1 class="mt-5 mb-5"><img src="../img/p24.png" width="100px"/></h1>
-    <form action="../index.php" method="post"><textarea id="c-code" name="code">
-      #include <stdio.h> 
+    <form action="../index.php" method="post"><textarea id="c-code" name="code">#include <stdio.h> 
       int main(void) {
       printf("Hello World!\n");
       return 0; 
